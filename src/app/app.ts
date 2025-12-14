9f519f613b6ef53a83600deb05e2381d504dd8fa
Mazhar Ali <sayedmazharali12@gmail.com>
2025-12-14 23:49:04 +0500

    Bismillah

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import {  RouterOutlet } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Header } from './shared/header/header';
 import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink , Home ,Header, Footer],
+  imports: [RouterOutlet , Home ,Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
